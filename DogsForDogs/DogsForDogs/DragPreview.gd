23417417a9aa90261430c75b829b307ecace98dc
HKait24 <164089710+HKait24@users.noreply.github.com>
2024-03-19 18:23:37 -0400

    Add files via upload

---
+++ DogsForDogs/DogsForDogs/DragPreview.gd
@@ -0,0 +1,10 @@
+extends Sprite
+
+func _ready():
+	pass 
+
+func _process(delta):
+	global_position = get_global_mouse_position()
+	
+	if Input.is_action_just_released("LeftMouse"):
+		queue_free()
